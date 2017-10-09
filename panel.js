96e2106776f7648aca9175ac43c64e20b49c4489
Strudy <aravnarula18@gmail.com>
2017-10-09 12:42:08 -0400

    Create panel.js

---
+++ panel.js
@@ -0,0 +1,3 @@
+  var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}})
+ +          findIP.then(ip => output('<span class="muted">' + ip + '</span>'))
+ +          break;
